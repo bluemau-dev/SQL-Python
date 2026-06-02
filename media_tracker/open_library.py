ec07740b6ce5e399e46adfa400d2e308e5b82bea
bluemau-dev <bluemau-dev@gmail.com>
2026-06-02 15:54:07 +0000

    Author not showing all books

--- media_tracker/open_library.py
+++ media_tracker/open_library.py
@@ -1,6 +1,7 @@
 import requests
+import sqlite3
 
-def search_book():
+def search_book(connection):
     
     # Prompt Input
     book_title = input("Enter the title of the book: ")
@@ -19,6 +20,9 @@ def search_book():
         print(f"Title: {book['title']}")
         print(f"Author: {book['author_name']}")
         print(f"Key: {book['author_key']}")
+
+        # Prompt User to Save
+        save_to_database(connection, book)
         
     except IndexError:
         print(f"The following book '{book_title}' could not be found or was spelled incorrectly.")
@@ -28,7 +32,7 @@ def search_book():
 def get_book_to_database():
     pass 
 
-def search_author():
+def search_author(connection):
 
     # Prompt Input
     author = input("Enter the authors first and last name: ")
@@ -46,11 +50,11 @@ def search_author():
 
         # Print Author Data
         print(f"Author: {author_data['name']}")
-        print(f"Top Subjects: {author_data['top_subjects']}")
+        #print(f"Top Subjects: {author_data['top_subjects']}")
         print(f"Key: {author_data['key']}")
 
         # Author's Books
-        get_author_works(key)
+        get_author_works(connection, author)
         
 
     except IndexError:
@@ -58,16 +62,18 @@ def search_author():
     except requests.exceptions.RequestException as e:
         print(f"Network error: {e}")
 
-def get_author_works(key):
+def get_author_works(connection, author):
     # API Endpoint
-    url = f'https://openlibrary.org/authors/{key}/works.json'
+    #url = f'https://openlibrary.org/authors/{key}/works.json'
+    url = f'https://openlibrary.org/search.json'
 
     try:
-        response = requests.get(url)
+        #response = requests.get(url)
+        response = requests.get(url, params={"author": author})
         response.raise_for_status()
 
         data = response.json()
-        books = data['entries']
+        books = data['docs']
 
         # Index and Data of the List
         for index, book in enumerate(books):
@@ -78,8 +84,8 @@ def get_author_works(key):
         
         # Prompt User to View Book Details
         choice = int(input("\nEnter the number of the book to see details: "))
-        if 0 <= choice <= len(books):
-            get_book_details(books[choice])
+        if 0 <= choice < len(books):
+            get_book_details(connection, books[choice])
         else:
             print("Invalid choice.")
 
@@ -88,26 +94,73 @@ def get_author_works(key):
     except requests.exceptions.RequestException as e:
         print(f"Network error: {e}")
 
-def get_book_details(book): # book -> Of Mice and Men
+def get_book_details(connection, book): # book -> Of Mice and Men
     # book['key'] -> "/works/OL45804W"
     url = f"https://openlibrary.org{book['key']}.json"
     
     try:   
-        response = requests.get(url).json()
+        response = requests.get(url)
         response.raise_for_status()
 
-        data = response.json()
+        book = response.json()
 
-        print(f"Title: {data.get('title')}")
-        print(f"Description: {data.get('description')}")
-        print(f"Subject: {data.get('subjects')}")
+        print(f"Title: {book.get('title')}")
+        print(f"Description: {book.get('description')}")
+        print(f"Subject: {book.get('subjects')}")
+
+        save = input("Save this book to database? y/n ")
+        if save.lower() == "y":
+            insert_book(connection, book)
+            print("Book saved.")
     
     except requests.exceptions.RequestException as e:
         print(f"Network error: {e}")
         
+def create_table(connection):
+    query = """
+    CREATE TABLE IF NOT EXISTS books(
+    media_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    author TEXT,
+    publish_year INTEGER,
+    work_key TEXT,
+    UNIQUE(title, publish_year)
+    )"""
+
+    try:
+        with connection: # Execute QUERY to create table
+            connection.execute(query)
+    except Exception as e:
+        print(e)
+
+def insert_book(connection, book):
+    query = """
+    INSERT OR IGNORE INTO books (title, author, publish_year, work_key)
+    VALUES (?,?,?,?)
+    """
+
+    # Data -> Values
+    title = book.get("title")
+    author = ", ".join(book.get("author_name", []))
+    publish_year = book.get("first_publish_year")
+    work_key = book.get("key")
+
+    # Execute Query
+    with connection:
+        connection.execute(query, (title,author,publish_year,work_key))
+
+def save_to_database(connection, book):
+
+    save = input("Save this book to database? y/n ")
+    if save.lower() == "y":
+        insert_book(connection, book)
+        print("Book saved.")
 
 def main():
     
+    connection = sqlite3.connect("books.db")
+    create_table(connection)
+
     options = {
         1: search_book,
         2: search_author,
@@ -117,7 +170,7 @@ def main():
     action = options.get(x)
 
     if action:
-        action()
+        action(connection)
     else:
         print("Invalid option.")
     
